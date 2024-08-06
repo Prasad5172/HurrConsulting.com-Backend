4c53890b847493ebd12a6699e0c48b8fa0181a52
Prasad5172 <prasadpadala2005@gmail.com>
2024-08-06 20:47:42 +0530

    small changes

--- server.js
+++ server.js
@@ -1,53 +1,57 @@
 var express = require("express");
 const cors = require("cors");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const path = require('path');
-const dayjs = require("dayjs")
-const fs = require("fs");
-const { authenticate } = require('@google-cloud/local-auth');
-const { google } = require('googleapis');
-const { v4: uuid } = require('uuid');
-const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
+const dayjs = require("dayjs");
+const { google } = require("googleapis");
+const { v4: uuid } = require("uuid");
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
 const bodyParser = require("body-parser");
 const { userRepository } = require("./repository/index.js");
-const {admin} = require("./middleware")
-// Configure CORS to allow requests from http://localhost:3000
+const { admin } = require("./middleware");
+
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: "http://localhost:3000",
 };
-app.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req, res) => {
-  console.log("webhook")
-  const sig = req.headers['stripe-signature'];
-  let event;
+app.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  async (req, res) => {
+    console.log("webhook");
+    const sig = req.headers["stripe-signature"];
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
-  } catch (err) {
-    console.log(err.message)
-    return res.status(400).send(`Webhook Error: ${err.message}`);
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(err.message);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    if (event.type === "payment_intent.succeeded") {
+      const paymentIntent = event.data.object;
+      const appointmentData = paymentIntent.metadata;
+
+      const auth = await authorize();
+      const eventData = {
+        summary: appointmentData.summary,
+        description: appointmentData.description,
+        start: { dateTime: appointmentData.start, timeZone: "Asia/Kolkata" },
+        end: { dateTime: appointmentData.end, timeZone: "Asia/Kolkata" },
+        attendees: [{ email: appointmentData.attendeeEmail }],
+      };
+
+      const calendarEvent = await addEvent(auth, eventData);
+      console.log("Calendar Event Created:", calendarEvent);
+    }
+    res.status(200).json({ received: true });
   }
-
-  if (event.type === 'payment_intent.succeeded') {
-    const paymentIntent = event.data.object;
-    const appointmentData = paymentIntent.metadata;
-
-    const auth = await authorize();
-    const eventData = {
-      summary: appointmentData.summary,
-      description: appointmentData.description,
-      start: { dateTime: appointmentData.start, timeZone: 'Asia/Kolkata' },
-      end: { dateTime: appointmentData.end, timeZone: 'Asia/Kolkata' },
-      attendees: [{ email: appointmentData.attendeeEmail }],
-    };
-
-    const calendarEvent = await addEvent(auth, eventData);
-    console.log('Calendar Event Created:', calendarEvent);
-  }
-
-  res.status(200).json({ received: true });
-});
+);
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -58,7 +62,10 @@ const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
 const calendarId = process.env.CALENDAR_ID;
 
 // Google Calendar API settings
-const SCOPES = ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/calendar.events"];
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar",
+  "https://www.googleapis.com/auth/calendar.events",
+];
 
 const auth = new google.auth.JWT(
   CREDENTIALS.client_email,
@@ -71,7 +78,7 @@ const auth = new google.auth.JWT(
 // const calendar = google.calendar({ version: "v3", auth });
 
 async function addEvent(auth, eventData) {
-  const calendar = google.calendar({ version: 'v3', auth });
+  const calendar = google.calendar({ version: "v3", auth });
   const response = await calendar.events.insert({
     calendarId: "primary",
     conferenceDataVersion: 1,
@@ -80,27 +87,26 @@ async function addEvent(auth, eventData) {
       description: eventData.description,
       start: {
         dateTime: dayjs(eventData.start).toISOString(),
-        timeZone: "Asia/Kolkata"
+        timeZone: "Asia/Kolkata",
       },
       end: {
         dateTime: dayjs(eventData.end).toISOString(),
-        timeZone:  "Asia/Kolkata"
+        timeZone: "Asia/Kolkata",
       },
       conferenceData: {
         createRequest: {
           requestId: uuid(),
         },
       },
-      attendees: eventData.attendees
-    }
+      attendees: eventData.attendees,
+    },
   });
   return response.data; // Return the event details including the event ID
 }
 
-
 async function updateEvent(auth, eventId, eventData) {
   console.log("i am in updateEvent");
-  const calendar = google.calendar({ version: 'v3', auth });
+  const calendar = google.calendar({ version: "v3", auth });
   const response = await calendar.events.update({
     calendarId: "primary",
     eventId: eventId,
@@ -109,54 +115,52 @@ async function updateEvent(auth, eventId, eventData) {
       description: eventData.description,
       start: {
         dateTime: eventData.start,
-        timeZone: eventData.timeZone || "Asia/Kolkata"
+        timeZone: eventData.timeZone || "Asia/Kolkata",
       },
       end: {
         dateTime: eventData.end,
-        timeZone: eventData.timeZone || "Asia/Kolkata"
+        timeZone: eventData.timeZone || "Asia/Kolkata",
       },
-      attendees: eventData.attendees
-    }
+      attendees: eventData.attendees,
+    },
   });
   return response.data; // Return the updated event details
 }
 
 async function deleteEvent(auth, eventId) {
-  console.log("i am in deleteEvent")
-  const calendar = google.calendar({ version: 'v3', auth });
+  console.log("i am in deleteEvent");
+  const calendar = google.calendar({ version: "v3", auth });
   await calendar.events.delete({
-    calendarId: 'primary',
+    calendarId: "primary",
     eventId: eventId,
   });
 }
 
 async function listEvents(auth) {
-  console.log("i am in listEvents")
-  const calendar = google.calendar({ version: 'v3', auth });
+  console.log("i am in listEvents");
+  const calendar = google.calendar({ version: "v3", auth });
   const res = await calendar.events.list({
-    calendarId: 'primary',
+    calendarId: "primary",
     timeMin: new Date().toISOString(),
     maxResults: 10,
     singleEvents: true,
-    orderBy: 'startTime',
+    orderBy: "startTime",
   });
   const events = res.data.items;
   return events;
 }
 
 async function getEventById(auth, eventId) {
-  // Use the Google Calendar API to fetch an event by its ID
-  // This is an example function; adjust it based on your actual implementation
-  const calendar = google.calendar({version: 'v3', auth});
+  //  Calendar API to fetch an event by its ID
+  const calendar = google.calendar({ version: "v3", auth });
   const response = await calendar.events.get({
-    calendarId: 'primary', // or the specific calendar ID you're working with
+    calendarId: "primary", // or the specific calendar ID you're working with
     eventId: eventId,
   });
   return response.data;
 }
 
-// Route to get event by ID
-app.get("/events/:id",admin, async (req, res) => {
+app.get("/events/:id", admin, async (req, res) => {
   const eventId = req.params.id;
   console.log(`Fetching event with ID: ${eventId}`);
   try {
@@ -172,15 +176,14 @@ app.get("/events/:id",admin, async (req, res) => {
   }
 });
 
-
-app.post("/event",admin, async (req, res) => {
-  console.log("admin post")
+app.post("/event", admin, async (req, res) => {
+  console.log("admin post");
   try {
     const event = await addEvent(auth, req.body);
     console.log(event);
     res.send({
       msg: "Event created successfully",
-      eventId: event.id // Send back the event ID
+      eventId: event.id, // Send back the event ID
     });
   } catch (error) {
     console.error("Error creating event", error);
@@ -188,15 +191,15 @@ app.post("/event",admin, async (req, res) => {
   }
 });
 
-app.put("/event/:eventId",admin, async (req, res) => {
-  console.log("admin put")
+app.put("/event/:eventId", admin, async (req, res) => {
+  console.log("admin put");
   try {
     const eventId = req.params.eventId;
     const event = await updateEvent(auth, eventId, req.body);
     console.log(event);
     res.send({
       msg: "Event updated successfully",
-      eventId: event.id // Send back the updated event ID
+      eventId: event.id, 
     });
   } catch (error) {
     console.error("Error updating event", error);
@@ -204,8 +207,8 @@ app.put("/event/:eventId",admin, async (req, res) => {
   }
 });
 
-app.delete("/event/:eventId",admin, async (req, res) => {
-  console.log("admin delete")
+app.delete("/event/:eventId", admin, async (req, res) => {
+  console.log("admin delete");
   try {
     await deleteEvent(auth, req.params.eventId);
     res.send({ msg: "Event deleted successfully" });
@@ -215,8 +218,8 @@ app.delete("/event/:eventId",admin, async (req, res) => {
   }
 });
 
-app.get("/events",admin, async (req, res) => {
-  console.log("admin get")
+app.get("/events", admin, async (req, res) => {
+  console.log("admin get");
   try {
     const events = await listEvents(auth);
     res.send(events);
@@ -226,37 +229,34 @@ app.get("/events",admin, async (req, res) => {
   }
 });
 
-
-
 app.post("/create-payment-intent", async (req, res) => {
-  console.log("create-payment-intent")
+  console.log("create-payment-intent");
   const { items } = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: 1000,
     currency: "usd",
-    
   });
-  console.log(paymentIntent.client_secret)
+  console.log(paymentIntent.client_secret);
   res.send({
     clientSecret: paymentIntent.client_secret,
   });
 });
 
-app.post('/api/bookevent', async (req, res) => {
+app.post("/api/bookevent", async (req, res) => {
   const { name, email, phone, problem, date, slot } = req.body;
 
   // Create payment intent with appointment details as metadata
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: 1000, // Amount in cents
-      currency: 'usd',
+      currency: "usd",
       metadata: {
         summary: `Appointment with ${name}`,
         description: problem,
-        start: new Date(date + 'T' + slot).toISOString(),
-        end: new Date(date + 'T' + slot).toISOString(), // Example duration
+        start: new Date(date + "T" + slot).toISOString(),
+        end: new Date(date + "T" + slot).toISOString(), // Example duration
         attendeeEmail: email,
       },
     });
@@ -269,16 +269,16 @@ app.post('/api/bookevent', async (req, res) => {
   }
 });
 
-
-app.get("/users",async (req,res) => {
-  await userRepository.retrieveAll((err,data) => {
-   if (err) {
-     return res.status(err.code).json(responseHandler(false, err.code, err.message, null));
- }
-   return res.status(200).json( data)
+app.get("/users", async (req, res) => {
+  await userRepository.retrieveAll((err, data) => {
+    if (err) {
+      return res
+        .status(err.code)
+        .json(responseHandler(false, err.code, err.message, null));
+    }
+    return res.status(200).json(data);
   });
- 
-})
+});
 
 app.listen(8000, () => {
   console.log("Server listening on port 8000");
